26113be4cd77a78130f1936e3ddf7f9dad464086
BarVinizky <barr98@gmail.com>
2025-08-20 18:23:14 +0300

    add configuration and query files

--- src/cohort_constractions.py
+++ src/cohort_constractions.py
@@ -9,74 +9,24 @@ import pandas as pd
 # from googleapiclient.discovery import build
 # from googleapiclient.http import MediaIoBaseDownload
 from typing import Dict, List, Tuple
+from query import *
+from config import *
 
-WINDOW_HOURS: int = 48
-GAP_HOURS: int = 6
-MIN_LOS_HOURS: int = 54
-MIN_AGE: int = 18
-MAX_AGE: int = 89
+# WINDOW_HOURS: int = 48
+# GAP_HOURS: int = 6
+# MIN_LOS_HOURS: int = 54
+# MIN_AGE: int = 18
+# MAX_AGE: int = 89
 
-PROLONGED_LOS_THRESHOLD_DAYS: int = 7
-READMISSION_WINDOW_DAYS: int = 30
-MORTALITY_WINDOW_DAYS: int = 30
+# PROLONGED_LOS_THRESHOLD_DAYS: int = 7
+# READMISSION_WINDOW_DAYS: int = 30
+# MORTALITY_WINDOW_DAYS: int = 30
 
-SECONDS_PER_HOUR = 60 * 60
-SECONDS_PER_YEAR = 365 * 24 * SECONDS_PER_HOUR
+# SECONDS_PER_HOUR = 60 * 60
+# SECONDS_PER_YEAR = 365 * 24 * SECONDS_PER_HOUR
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-
-ICUQ = f"""
-    SELECT a.subject_id::INTEGER AS subject_id,
-           a.hadm_id::INTEGER AS hadm_id,
-           a.admittime::TIMESTAMP AS admittime,
-           a.dischtime::TIMESTAMP AS dischtime,
-           a.admission_type AS admission_type,
-           a.admission_location AS admission_location,
-           a.insurance AS insurance,
-           a.language AS language,
-           a.religion AS religion,
-           a.marital_status AS marital_status,
-           a.ethnicity AS ethnicity,
-           a.edregtime::TIMESTAMP AS edregtime,
-            --    a.diagnosis AS diagnosis,
-           a.has_chartevents_data::INTEGER AS has_chartevents_data,
-           p.gender AS gender,
-           p.dob::TIMESTAMP AS dob,
-           p.dod::TIMESTAMP AS dod
-    FROM admissions a
-    JOIN patients p ON a.subject_id = p.subject_id
-    WHERE a.subject_id::INTEGER IN (SELECT subject_id FROM tmp_subject_ids)
-    """
-
-LABQUERY = f"""
-    SELECT l.subject_id::INTEGER AS subject_id,
-           l.hadm_id::INTEGER AS hadm_id,
-           l.charttime::TIMESTAMP AS charttime,
-           l.itemid::INTEGER AS itemid,
-           l.valuenum::DOUBLE AS valuenum
-    FROM labevents l
-    JOIN admissions a ON l.subject_id = a.subject_id AND l.hadm_id = a.hadm_id
-    WHERE l.itemid::INTEGER IN (SELECT itemid FROM tmp_lab_itemids)
-      AND l.hadm_id::INTEGER IN (SELECT hadm_id FROM tmp_hadm_ids)
-      AND l.charttime::TIMESTAMP BETWEEN a.admittime::TIMESTAMP AND a.admittime::TIMESTAMP + INTERVAL {WINDOW_HOURS} HOURS
-      AND l.valuenum IS NOT NULL
-    """
-
-VITQUER = f"""
-    SELECT c.subject_id::INTEGER AS subject_id,
-           c.hadm_id::INTEGER AS hadm_id,
-           c.charttime::TIMESTAMP AS charttime,
-           c.itemid::INTEGER AS itemid,
-           c.valuenum::DOUBLE AS valuenum
-    FROM chartevents c
-    JOIN admissions a ON c.subject_id = a.subject_id AND c.hadm_id = a.hadm_id
-    WHERE c.itemid::INTEGER IN (SELECT itemid FROM tmp_vital_itemids)
-      AND c.hadm_id::INTEGER IN (SELECT hadm_id FROM tmp_hadm_ids)
-      AND c.charttime::TIMESTAMP BETWEEN a.admittime::TIMESTAMP AND a.admittime::TIMESTAMP + INTERVAL {WINDOW_HOURS} HOURS
-      AND c.valuenum IS NOT NULL
-      AND c.error::INTEGER == 0
-    """
 
 def load_initial_subjects(path: str) -> List[int]:
     """Load the initial cohort subject IDs from CSV."""
